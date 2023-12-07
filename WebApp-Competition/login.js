fe785236b725aca7d693e5b08fa0717801b204bc
sosegadamente <149854564+sosegadamente@users.noreply.github.com>
2023-12-07 20:13:42 +0900

    move

--- WebApp-Competition/login.js
+++ WebApp-Competition/login.js
@@ -4,10 +4,11 @@ const checkAgain = () => {
     const inputPwValue = document.getElementById('inputpw').querySelector('input').value.trim();
 
     if (inputIdValue === "") {
-        alert('아이디를 입력해주세요');
+        alert('아이디를 입력해주세요.');
     } else if (inputPwValue === "") {
-        alert('비밀번호를 입력해주세요');
+        alert('비밀번호를 입력해주세요.');
     } else {
-        console.log(1);
+        alert('로그인에 실패했습니다. 메인페이지로 이동합니다.')
+        window.location.href = 'mainPage.html';
     }
 }
